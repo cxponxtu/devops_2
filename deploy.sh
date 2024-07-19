f4aa8877c4172243456f1199c859044347b4510b
Revanth S <Revanth8940880750@gmail.com>
2024-07-19 11:37:03 +0000

    update

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,28 @@
 #! /bin/sh
 
-# Pulling images
-docker pull revanth7733/user_service:latest
-docker pull revanth7733/product_service:latest
-docker pull revanth7733/order_service:latest
+# Timestamp
+echo "`date +'%d-%m-%y_%H_%M_%S'`\nStart of log\n" >> deploy.log
 
-# Starting compose
-docker compose up
+# Pulling images
+echo "Puling images..."
+docker compose pull 2>&1 | cat >> deploy.log
+if [ $? -eq 0 ]
+then
+    echo "Images pulled successfully."
+else
+    echo "Error pulling images. See deploy.log for more details."
+    exit 1
+fi
+
+# Restarting services
+echo "Restarting services..."
+docker compose up -d --build 2>&1 | cat >> deploy.log
+if [ $? -eq 0 ]
+then
+    echo "Services restarted successfully."
+else
+    echo "Error restarting services. See deploy.log for more details."
+    exit 1
+fi
+
+echo "\nEnd of log file.\n\n\n" >> deploy.log
